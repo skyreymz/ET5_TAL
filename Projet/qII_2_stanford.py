ca97013cb20a90a1da4591f9cc6eb39500fa1c26
TMWilliam <tri-man-william.vu@u-psud.fr>
2022-02-26 09:52:34 +0100

    Writing for delivery

--- Projet/qII_2_stanford.py
+++ Projet/qII_2_stanford.py
@@ -82,3 +82,23 @@ for l in lines:
 			raise ValueError("L'entite nommee possede un caractère / dans son nom")
 
 fw.close()
+
+
+
+"""
+Description de l'algorithme :
+
+Les entités nommées obtenues par le NE recognizer de STANFORD ne sont pas données sous forme d’arbre syntaxique, contrairement au NE recognizer de NLTK.
+En effet, pour STANFORD, nous obtenons à la suite des tokens avec leur étiquette respective au lieu d'avoir une structure en arbre syntaxique pouvant être composé de plusieurs tokens à la fois pour une seule même étiquette.
+Par exemple, "Paul Itteck" est décomposé en deux tokens “Paul” et “Itteck” distincts ayant chacun pour étiquette PERSON, au lieu d'être décomposé comme un arbre syntaxique possédant les deux tokens “Paul” et “Itteck” en tant que nœuds de l’arbre et l’étiquette PERSON en tant que racine de l’abre.
+Ainsi, pour NLTK, il suffit de récupérer les nœuds de l’arbre pour obtenir les différents mots de l’entité nommée.
+Le NE recognizer de STANFORD se contente quant à lui d’associer à un token, une étiquette d’une entité nommée s’il le juge comme telle, sans faire de lien entre les précédents tokens alors qu’ils pourraient ensemble, représenter une seule même entité nommée. 
+Ainsi, pour les résultats obtenus avec le NE recognizer de STANFORD, nous ne pouvons pas en déduire à vue d’œil les débuts d'entités (ex: B-ORG) des suites d'entités (ex: I-ORG).
+
+Il faudra donc trouver une stratégie afin de convertir les étiquettes actuelles par les étiquettes CoNLL-2003 nous permettant de voir si le token s’agit d’un début d’entité nommée (B-) ou de sa suite (I-).
+Cette stratégie sera donc de détecter lorsqu’une entité nommée est rencontrée puis d'analyser le token qui suit :
+- (*) Si le token qui suit est une entité avec la même étiquette que la précédente, alors ce token est une suite de l'entité précédemment rencontrée
+- Si le token qui suit est une entité mais avec une étiquette différente de la précédente, alors ce token est une autre entité
+- Sinon (donc si le token est étiqueté en tant que "OTHER"), le token n'est pas une entité.
+(*) Cette stratégie n'est correcte que si l'on considère de plus, que deux entités différentes mais qui ont la même étiquette (par exemple, deux PERSON différentes mais qui sont donc deux entités avec la meme etiquette), sont au moins séparées par un token qui n'est pas une entité. 
+"""
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ET5_TAL
+
+Pour executer les commandes bash des TP1, TP2 et du Projet, veuillez vous placer dans le dossier correspondant.
